0a72a0e64e56a106c9e09338889cc24100b99ccb
Kenneth Jiang <kenneth.jiang@gmail.com>
2016-03-15 00:00:46 +0000

    Surpress errors when S3 connection has errors

--- s3_policy_sync/middleware.py
+++ s3_policy_sync/middleware.py
@@ -2,6 +2,9 @@ from django.core.exceptions import MiddlewareNotUsed
 from django.conf import settings
 import boto
 import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 def setting(name, default=None):
     """
@@ -14,13 +17,16 @@ def setting(name, default=None):
 class S3PolicySyncMiddleware(object):
 
     def __init__(self):
-        s3 = boto.connect_s3()
-        policy_dir = getattr(settings, 'S3_POLICY_DIR')
+        try:
+            s3 = boto.connect_s3()
+            policy_dir = getattr(settings, 'S3_POLICY_DIR')
 
-        for bucket_name in getattr(settings, 'S3_BUCKETS', []):
-            bucket = s3.get_bucket(bucket_name)
-            policy = open(os.path.join(policy_dir, bucket_name)).read()
-            bucket.set_policy(policy)
+            for bucket_name in getattr(settings, 'S3_BUCKETS', []):
+                bucket = s3.get_bucket(bucket_name)
+                policy = open(os.path.join(policy_dir, bucket_name)).read()
+                bucket.set_policy(policy)
+        except:
+            logger.warning('Skipped synchronizing S3 policies because of errors')
 
         # This middleware should only run once at the startup.
         # see http://stackoverflow.com/questions/2781383/where-to-put-django-startup-code
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-s3-policy-sync',
-      version='0.1.0',
+      version='0.2.0',
       description='Django middleware that synchronizes S3 policy to from files',
       url='https://github.com/Rhumbix/django-s3-policy-sync.git',
       author='Kenneth Jiang',
